879a223dd41edadbfffd803f0842d1aa41f591ff
Jyotirmoy <jyotirmoykarmakar02@gmail.com>
2024-02-08 00:14:50 +0530

    card actions

---
+++ components/Actions.jsx
@@ -0,0 +1,81 @@
+"use client";
+
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "./ui/dropdown-menu";
+import { Link2, Pencil, Trash2 } from "lucide-react";
+import { toast } from "sonner";
+import { useApiMutation } from "@/hooks/useApiMutation";
+import { api } from "@/convex/_generated/api";
+import ConfirmModal from "./ConfirmModal";
+import { Button } from "./ui/button";
+import { useRenameModal } from "@/store/useRenameModal";
+
+const Actions = ({ children, side, sideOffset, id, title }) => {
+  const { mutate, pending } = useApiMutation(api.board.remove);
+  const { onOpen } = useRenameModal();
+
+  const onCopyLink = () => {
+    navigator.clipboard
+      .writeText(`${window.location.origin}/board/${id}`)
+      .then(() => toast.success("Link copied"))
+      .catch(() => toast.error("Failed to copy link"));
+  };
+
+  const onDelete = () => {
+    mutate({ id })
+      .then(() => toast.success("Board deleted"))
+      .catch(() => toast.error("Failed to delte board"));
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+
+      <DropdownMenuContent
+        side={side}
+        sideOffset={sideOffset}
+        className='w-60'
+        onClick={(e) => e.stopPropagation()}
+      >
+        <DropdownMenuItem className='p-3 cursor-pointer' onClick={onCopyLink}>
+          <Link2 className='h-4 w-4 mr-2 ' />
+          Copy board link
+        </DropdownMenuItem>
+
+        <DropdownMenuItem
+          onClick={() => {
+            console.log("clicked");
+            onOpen(id, title);
+          }}
+          className='p-3 cursor-pointer'
+        >
+          <Pencil className='h-4 w-4 mr-2' />
+          Rename
+        </DropdownMenuItem>
+
+        <ConfirmModal
+          header='Delete board?'
+          description='This will delete the board and all of its contents'
+          disabled={pending}
+          onConfirm={onDelete}
+        >
+          <Button
+            variant='ghost'
+            className='p-3 cursor-pointer text-sm w-full justify-start font-normal'
+          >
+            <Trash2 className='h-4 w-4 mr-2 ' />
+            Delete
+          </Button>
+        </ConfirmModal>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default Actions;
---
+++ components/ConfirmModal.jsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTrigger,
+  AlertDialogTitle,
+} from "./ui/alert-dialog";
+
+const ConfirmModal = ({
+  children,
+  onConfirm,
+  disabled,
+  header,
+  description,
+}) => {
+  const handleConfirm = () => {
+    onConfirm();
+  };
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{header}</AlertDialogTitle>
+
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction disabled={disabled} onClick={handleConfirm}>
+            Confirm
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default ConfirmModal;
